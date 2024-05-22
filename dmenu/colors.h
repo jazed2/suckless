a411f5e5c426b48f7feb1a534effec67601bb547
jazed2 <jazze.martine@gmail.com>
2024-05-22 09:53:54 +0530

    Applied patch 'dmenu-bordercolor-20230512-0f3460d.diff'

--- dmenu/colors.h
+++ dmenu/colors.h
@@ -3,4 +3,5 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#696969", "#000000" },
 	[SchemeSel] = { "#FFFFFF", "#000000" },
 	[SchemeOut] = { "#000000", "#FFFFFF" },
+	[SchemeBorder] = { "#696969", "#B00000" },
 };
--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 1000;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
@@ -12,10 +12,13 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 
 #include "colors.h"
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 7;
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 1;
