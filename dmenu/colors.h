25fdb8062f99975234cb6a89ab99dae45b885ce0
jazed2 <jazze.martine@gmail.com>
2024-05-23 07:27:18 +0530

    patch reversion

--- dmenu/colors.h
+++ dmenu/colors.h
@@ -3,7 +3,9 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#696969", "#000000" },
 	[SchemeSel] = { "#FFFFFF", "#000000" },
 	[SchemeOut] = { "#000000", "#FFFFFF" },
+	//Border color
 	[SchemeBorder] = { "#696969", "#B00000" },
+	//Highlight color
 	[SchemeSelHighlight] = { "#ffc978", "#005577" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
